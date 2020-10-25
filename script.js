80cd8db6b747621bb3936fed08279703744b4807
Wilson Fong <wilsonfong82@gmail.com>
2020-10-24 23:14:21 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+import { jsPDF } from "./node_modules/jspdf/dist/jspdf.es.js";
+
+
+// Default export is a4 paper, portrait, using millimeters for units
+const doc = new jsPDF();
+ 
+doc.text("Hello world!", 10, 10);
+doc.save("a4.pdf");
